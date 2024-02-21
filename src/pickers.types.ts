dd68bf8247db459a242d10c3219fe22a43d37d8e
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-02-20 18:38:46 -0800

    first commit

---
+++ src/pickers.types.ts
@@ -0,0 +1,34 @@
+export enum BloodType {
+    A_POSITIVE = "A+",
+    A_NEGATIVE = "A-",
+    B_POSITIVE = "B+",
+    B_NEGATIVE = "B-",
+    O_POSITIVE = "O+",
+    O_NEGATIVE = "O-",
+    AB_POSITIVE = "AB+",
+    AB_NEGATIVE = "AB-",
+  }
+  
+  export enum Relationship {
+    FAMILY = "Family",
+    FRIEND = "Friend",
+    COLLEAGUE = "Colleague",
+    OTHER = "Other",
+  }
+  
+  export interface IPickerContact {
+    name: string;
+    phone: string;
+    relationship: Relationship;
+  }
+  
+  export interface IPicker {
+    id: string;
+    name: string;
+    phone: string;
+    emergencyContact: IPickerContact;
+    govId: string;
+    bloodType: BloodType;
+    address: string;
+  }
+  
